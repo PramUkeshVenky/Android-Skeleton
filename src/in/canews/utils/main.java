dad682939f7fae1c2ce47b1f5e861ad1c5bb04c5
PramUkeshVenky <n.bhargavvenky@gmail.com>
2018-03-01 10:38:45 +0530

    added app level gradle file

--- src/in/canews/utils/main.java
+++ src/in/canews/utils/main.java
@@ -52,8 +52,74 @@ public class main {
         createGradlewbat(dir, projectName);
         createlocalProperties(dir, projectName);
         createsettingsGradle(dir, projectName);
+        createGradleBuidforapp(projectDir, packageName);
 //    createGradleProperties(dir,projectName);
-//    createGradleProperties(dir,projectName);
+    }
+
+    public void createGradleBuidforapp(String projectDir, String packageName) throws IOException {
+        File f = new File(projectDir + "//app//build.gradle");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter minSdkVersion Here : ");
+        String minSdkVersion = scanner.nextLine();
+
+        String s = "apply plugin: 'com.android.application'\n"
+                + "\n"
+                + "android {\n"
+                + "    compileSdkVersion 26\n"
+                + "    defaultConfig {\n"
+                + "        applicationId \""
+                + packageName
+                + "\"\n"
+                + "        minSdkVersion "
+                + minSdkVersion
+                + "\n"
+                + "        targetSdkVersion 26\n"
+                + "        versionCode 1\n"
+                + "        versionName \"1.0\"\n"
+                + "    }\n"
+                + "    buildTypes {\n"
+                + "        release {\n"
+                + "            minifyEnabled false\n"
+                + "            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'\n"
+                + "        }\n"
+                + "    }\n"
+                + "}\n"
+                + "\n"
+                + "dependencies {\n"
+                + "    implementation fileTree(dir: 'libs', include: ['*.jar'])\n"
+                + "	// add dependencies here\n"
+                + "}";
+        FileWriter w = new FileWriter(f);
+        w.write(s);
+        w.close();
+
+        File fn = new File(projectDir + "//app//proguard-rules.pro");
+
+        String st = "# Add project specific ProGuard rules here.\n"
+                + "# You can control the set of applied configuration files using the\n"
+                + "# proguardFiles setting in build.gradle.\n"
+                + "#\n"
+                + "# For more details, see\n"
+                + "#   http://developer.android.com/guide/developing/tools/proguard.html\n"
+                + "\n"
+                + "# If your project uses WebView with JS, uncomment the following\n"
+                + "# and specify the fully qualified class name to the JavaScript interface\n"
+                + "# class:\n"
+                + "#-keepclassmembers class fqcn.of.javascript.interface.for.webview {\n"
+                + "#   public *;\n"
+                + "#}\n"
+                + "\n"
+                + "# Uncomment this to preserve the line number information for\n"
+                + "# debugging stack traces.\n"
+                + "#-keepattributes SourceFile,LineNumberTable\n"
+                + "\n"
+                + "# If you keep the line number information, uncomment this to\n"
+                + "# hide the original source file name.\n"
+                + "#-renamesourcefileattribute SourceFile";
+        FileWriter wr = new FileWriter(fn);
+        wr.write(st);
+        wr.close();
     }
 
     public void createDirs(String projectName, String packageName, String projectDir) throws IOException {
@@ -71,9 +137,9 @@ public class main {
         Boolean i = new File(projectName + "/app/src/main/java").mkdir();
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter MainActivity File Name(With out .java) : ");
+        System.out.print("Enter MainActivity File Name(With out .java Eg:MainActivity) : ");
         String name = sc.nextLine();
-        System.out.print("Enter MainLayoutName File Name(With out .xml) : ");
+        System.out.print("Enter MainLayoutName File Name(With out .xml Eg:activity_main) : ");
         String namexml = sc.nextLine();
 
         File fn = new File(projectDir + "//app//src//main//AndroidManifest.xml");
